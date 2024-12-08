7332658f2f90c43fd8fdd23085851b0182027fa2
Plague Fox <plugfox@gmail.com>
2024-12-09 01:07:46 +0400

    Update benchmark

--- benchmarks/iterator_benchmark.dart
+++ benchmarks/iterator_benchmark.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: avoid_print
-
 import 'dart:typed_data';
 
 import 'package:benchmark_harness/benchmark_harness.dart';
@@ -15,7 +14,10 @@ Benchmark 2dList: 16965.59 us
 
 const int _listLength = 1000000;
 
-// dart run benchmarks/iterator_benchmark.dart
+// $ dart run benchmarks/iterator_benchmark.dart
+//
+// $ dart compile exe -o benchmarks/iterator_benchmark.exe benchmarks/iterator_benchmark.dart
+// $ benchmarks/iterator_benchmark.exe
 void main() {
   (<BenchmarkBase>[
     _MutableIntListBenchmark(),
@@ -23,14 +25,16 @@ void main() {
     _ListListBenchmark(),
     _ObjectListBenchmark(),
     _Uint32ListBenchmark(),
+    _Uint64ListBenchmark(),
     _ExtensionListBenchmark(),
+    _MapListBenchmark(),
+    _RecordListBenchmark(),
   ].map<({String name, double us})>(_measure).toList(growable: false)
         ..sort((a, b) => a.us.compareTo(b.us)))
       .map<String>((e) => 'Benchmark ${e.name}: ${e.us.toStringAsFixed(2)} us')
       .forEach(print);
 }
 
-@pragma('vm:never-inline')
 ({String name, double us}) _measure(BenchmarkBase benchmark) =>
     (name: benchmark.name, us: benchmark.measure());
 
@@ -95,6 +99,26 @@ class _Uint32ListBenchmark extends BenchmarkBase {
   }
 }
 
+class _Uint64ListBenchmark extends BenchmarkBase {
+  _Uint64ListBenchmark() : super('Uint64List');
+
+  final Uint64List _list = Uint64List(_listLength);
+
+  @override
+  void setup() {
+    super.setup();
+    for (var i = 0; i < _listLength; i++) _list[i] = i;
+  }
+
+  @override
+  void run() {
+    var result = 0;
+    for (var i = 0; i < _list.length; i++) result += _list[i];
+    if (result != _listLength * (_listLength - 1) ~/ 2)
+      throw StateError('Incorrect result: $result');
+  }
+}
+
 class _ObjectListBenchmark extends BenchmarkBase {
   _ObjectListBenchmark() : super('ObjectList');
 
@@ -116,6 +140,13 @@ class _ObjectListBenchmark extends BenchmarkBase {
   }
 }
 
+final class _EntityObject {
+  _EntityObject(this.id);
+
+  /// Internal identifier.
+  final int id;
+}
+
 class _ListListBenchmark extends BenchmarkBase {
   _ListListBenchmark() : super('2dList');
 
@@ -135,13 +166,6 @@ class _ListListBenchmark extends BenchmarkBase {
     if (result != _listLength * (_listLength - 1) ~/ 2)
       throw StateError('Incorrect result: $result');
   }
-}
-
-final class _EntityObject {
-  _EntityObject(this.id);
-
-  /// Internal identifier.
-  final int id;
 }
 
 class _ExtensionListBenchmark extends BenchmarkBase {
@@ -168,3 +192,74 @@ class _ExtensionListBenchmark extends BenchmarkBase {
 extension type _EntityExtension(int _id) implements int {
   int get id => _id;
 }
+
+class _MapListBenchmark extends BenchmarkBase {
+  _MapListBenchmark() : super('MapList');
+
+  final List<Map<String, dynamic>> _list = <Map<String, dynamic>>[];
+
+  @override
+  void setup() {
+    super.setup();
+    _list.clear();
+    for (var i = 0; i < _listLength; i++) _list.add({'id': i});
+  }
+
+  @override
+  void run() {
+    var result = 0;
+    for (var i = 0; i < _list.length; i++) result += _list[i]['id'] as int;
+    if (result != _listLength * (_listLength - 1) ~/ 2)
+      throw StateError('Incorrect result: $result');
+  }
+}
+
+class _RecordListBenchmark extends BenchmarkBase {
+  _RecordListBenchmark() : super('RecordList');
+
+  final List<({int id})> _list = <({int id})>[];
+
+  @override
+  void setup() {
+    super.setup();
+    _list.clear();
+    for (var i = 0; i < _listLength; i++) _list.add((id: i));
+  }
+
+  @override
+  void run() {
+    var result = 0;
+    for (var i = 0; i < _list.length; i++) result += _list[i].id;
+    if (result != _listLength * (_listLength - 1) ~/ 2)
+      throw StateError('Incorrect result: $result');
+  }
+}
+
+/* class _StructureListBenchmark extends BenchmarkBase {
+  _StructureListBenchmark() : super('StructureList');
+
+  final List<ffi.Pointer<_EntityStruct>> _list = <ffi.Pointer<_EntityStruct>>[];
+
+  @override
+  void setup() {
+    super.setup();
+    _list.clear();
+    for (var i = 0; i < _listLength; i++) {
+      final ptr = ffi.calloc<_EntityStruct>();
+      _list.add(ptr..ref.id = i);
+    }
+  }
+
+  @override
+  void run() {
+    var result = 0;
+    for (var i = 0; i < _list.length; i++) result += _list[i].ref.id;
+    if (result != _listLength * (_listLength - 1) ~/ 2)
+      throw StateError('Incorrect result: $result');
+  }
+}
+
+final class _EntityStruct extends ffi.Struct {
+  @ffi.Uint32()
+  external int id;
+} */
